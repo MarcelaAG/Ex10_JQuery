5e28cfe38a76875133e905acd2489e0f8171bf8c
MarcelaAG <marcela.aguilerablanc@gmail.com>
2021-05-20 09:32:27 +0200

    add a class to an element

---
+++ main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    //The addClass() method adds one or more class names to the selected elements.
+   // This method does not remove existing class attributes, it only adds one or more class names to the class attribute.
+    let addClass = $('#texte_3');
+    $(addClass).addClass('a_cacher') 
+    $('.a_cacher').hide();
+});    
